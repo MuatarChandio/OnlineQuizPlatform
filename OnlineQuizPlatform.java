9d8f0b815f6093ce04f3a020374bf63acc19ab71
Muatar Chandio <muatarchandio@gmail.com>
2025-01-15 18:29:36 +0500

    added Code

---
+++ OnlineQuizPlatform.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class OnlineQuizPlatform{
+	public static void main(String arg[]){
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Welcome to Online Quiz Platform");
+	}
+
+}
